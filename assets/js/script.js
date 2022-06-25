9497f478c3b1eed0295c898b49e3c4851761ca28
lili <o7lilip@gmail.com>
2022-06-24 22:11:17 -0400

    html complete and some css styling done

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+// when start button is clicked, question and 4 answer choices appear from questions array
+// if correct answer is chosen, "correct!" appears in answer-footer and next question/answer choices appear
+// else "wrong!" appears in answer-footer and next question/answer choices appear
+// loop back through questions array until finished
