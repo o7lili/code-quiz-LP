c5a071016061dd11ec203aa3b5eb1d0407a9ee1b
lili <o7lilip@gmail.com>
2022-06-26 20:17:30 -0400

    added more variables and functions. next question appears when answer is chose, correct or wrong is displayed accordingly. need to fix question array error, and card for score initials input not displaying correctly

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,108 @@
 var timerEl = document.querySelector('.timer');
 var scoresEl = document.querySelector('.high-scores');
 var startBtn = document.getElementById('start-btn');
+var quizInfo = document.getElementById('quiz-instructions');
 var answerBtns = document.querySelectorAll('.answer');
+var questionEl = document.getElementById('card-header');
+var answerBtnsEl = document.getElementById('answer-btns');
+var finalScore = document.getElementById('final-score');
+var answerConfirm = document.getElementById('correct-wrong');
+var submitBtn = document.getElementById('submit-btn');
+var scoreText = document.getElementById('score');
+
+let randomQuestions, currentQuestionIndex
+
+startBtn.addEventListener('click', startQuiz);
+answerBtnsEl.addEventListener('click', () => {
+    currentQuestionIndex++;
+    setNextQuestion();
+});
+
+
+function startQuiz() {
+    countdown();
+    console.log('started');
+    startBtn.classList.add('hide');
+    randomQuestions = questions.sort(() => Math.random() - .5);
+    currentQuestionIndex = 0;
+    quizInfo.classList.add('hide');
+    answerBtns.forEach(answer => {
+        // remove class from each element
+        answer.classList.remove('hide')
+    });
+    setNextQuestion();
+};
+
+function countdown() {
+    var timeLeft = 80;
+
+    var timeInterval = setInterval( function () {
+        if (timeLeft > 1) {
+            timeLeft--;
+        }
+        else {
+            clearInterval(timeInterval);
+            displayMessage();
+        }
+    }, 1000);
+}
+
+function setNextQuestion() {
+    resetState();
+    showQuestion(randomQuestions[currentQuestionIndex]);
+};
+
+function showQuestion(question) {
+    questionEl.innerText = question.question;
+    question.answers.forEach(answer => {
+        var button = document.createElement('button');
+        button.innerText = answer.text;
+        button.classList.add('btn');
+        if(answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer);
+        answerBtnsEl.appendChild(button);
+    });
+};
+
+function resetState() {
+    while (answerBtnsEl.firstChild) {
+        answerBtnsEl.removeChild(answerBtnsEl.firstChild);
+    };
+};
+
+function selectAnswer(e) {
+    var selectedBtn = e.target;
+    var correct = selectedBtn.dataset.correct;
+    setStatusClass(document.body, correct);
+    Array.from(answerBtnsEl.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct);
+    });
+
+    if (randomQuestions.length > currentQuestionIndex + 1) {
+       setNextQuestion();
+    }
+    else {
+        questionEl.classList.add('hide');
+        answerBtnsEl.classList.add('hide');
+        finalScore.classList.remove('hide');
+        answerConfirm.classList.add('hide');
+        submitBtn.classList.remove('hide');
+        scoreText.innerHTML = "<p>Your final score is " + timer + ".</p>"; 
+    };
+};
+
+function setStatusClass(element, correct) {
+    if (correct) {
+        answerConfirm.innerHTML = "Correct!";
+        element.classList.add('correct');
+    }
+    else {
+        answerConfirm.innerHTML = "Wrong!";
+        element.classList.add('wrong');
+    }
+};
 
 var questions = [
     {
@@ -20,28 +121,45 @@ var questions = [
             {text: '3. alerts', correct: true},
             {text: '4. numbers', correct: false},
         ]
-    }
-]
+    },
 
-startBtn.addEventListener('click', startQuiz);
-// var correctEl = document.querySelector()  for when correct answer is clicked
-// var wrongEl = document.querySelector()  for when wrong answer is clicked
+    {
+        question: 'The condition in an if/else statement is enclosed with _____.',
+        answers: [
+            {text: '1. quotes', correct: false},
+            {text: '2. curly brackets', correct: false},
+            {text: '3. parenthesis', correct: true},
+            {text: '4. square brackets', correct: false},
+        ]
+    },
+    
+    {
+        question: 'Arrays in JavaScript can be used to store _____.',
+        answers: [
+            {text: '1. numbers and strings', correct: false},
+            {text: '2. other arrays', correct: false},
+            {text: '3. booleans', correct: false},
+            {text: '4. all of the above', correct: true},
+        ]
+    },
 
+    {
+        question: 'String values must be enclosed within _____ when being assigned to variables',
+        answers: [
+            {text: '1. commas', correct: false},
+            {text: '2. curly brackets', correct: false},
+            {text: '3. quotes', correct: true},
+            {text: '4. parenthesis', correct: false},
+        ]
+    },
 
-function startQuiz() {
-    console.log('started');
-    startBtn.classList.add('hide');
-    answerBtns.forEach(answer => {
-        // remove class from each element
-        answer.classList.remove('hide')
-    });
-    setNextQuestion();
-}
-
-function setNextQuestion() {
-
-}
-
-function selectAnswer() {
-
-}
+    {
+        question: 'A very useful tool used during development and debugging for printing content to the debugger is:',
+        answers: [
+            {text: '1. JavaScript', correct: false},
+            {text: '2. terminal/bash', correct: false},
+            {text: '3. for loops', correct: false},
+            {text: '4. console.log', correct: true},
+        ]
+    },
+];
